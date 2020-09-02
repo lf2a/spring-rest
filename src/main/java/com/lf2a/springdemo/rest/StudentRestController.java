f9d0973843ced3e00beafa6f956aadb2cd6739c9
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-09-02 12:51:45 -0300

    Refactoring code

--- src/main/java/com/lf2a/springdemo/rest/StudentRestController.java
+++ src/main/java/com/lf2a/springdemo/rest/StudentRestController.java
@@ -5,22 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> studentList;
 
-        List<Student> studentList = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+        studentList = new ArrayList<>();
 
         studentList.add(new Student("Luiz", "Filipy"));
         studentList.add(new Student("Maria", "Celina"));
         studentList.add(new Student("Pedro", "José"));
         studentList.add(new Student("Leticia", "Costa"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return studentList;
     }
 }
