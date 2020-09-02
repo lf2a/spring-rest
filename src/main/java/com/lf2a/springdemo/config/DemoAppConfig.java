ff737b2a7430ee8e8023158fcf17939b7f4dc340
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-09-02 12:16:21 -0300

    hello world

---
+++ src/main/java/com/lf2a/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.lf2a.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.lf2a.springdemo")
+public class DemoAppConfig implements WebMvcConfigurer {
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: luiz
+  Date: 02/09/2020
+  Time: 12:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spring REST</title>
+</head>
+<body>
+<h1>Spring REST Demo</h1>
+<a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+</body>
+</html>
